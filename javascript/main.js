d89f3b5616a66cced5468a36cea9c5055757b0ff
Ell Parker <miss.ellparker@gmail.com>
2017-08-09 21:48:15 -0700

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,20 @@
+var array = ["stuff"]
+
+array.push("more stuff", "something else")
+
+var obj = {
+  prop1:"value",
+  prop2:"other value",
+  prop3:"no comment"
+}
+
+//constructor function
+
+function ObjCreate(){
+  this.prop1 = value1
+  this.prop2 = value2
+  this.prop3 = value3
+  this.method = function () {
+    
+  }
+}
